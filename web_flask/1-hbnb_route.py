d6dd84fe781e6090323399fa6d5143556a22428a
GeorgeGE7 <elgeo.e.ge.7@gmail.com>
2024-03-23 01:18:14 +0200

    Add/Modify

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-two routes and flask init
+starts a Flask web application
 """
 
 from flask import Flask
@@ -8,13 +8,15 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def home():
-    """home route with simple string"""
+def index():
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
-app.route("/hbnb", strict_slashes=False)
+
+
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """seconde route for hbnb"""
-    return "HBNB"
+    """returns HBNB"""
+    return 'HBNB'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
