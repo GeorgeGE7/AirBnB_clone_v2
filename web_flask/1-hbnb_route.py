ee468160acc4413a03a6070be96fb3be73a64c1e
GeorgeGE7 <elgeo.e.ge.7@gmail.com>
2024-03-23 01:16:01 +0200

    Seconde file

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 """
-first route and flask init
+two routes and flask init
 """
 
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def home():
     """home route with simple string"""
     return 'Hello HBNB!'
+
+def hbnb():
+    """seconde route for hbnb"""
+    return "HBNB"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
