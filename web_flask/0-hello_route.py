344dfdb9b6ddcca12e98da2ff37953b070e6919e
GeorgeGE7 <elgeo.e.ge.7@gmail.com>
2024-03-23 01:02:12 +0200

    Modify First Route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,22 @@
+#!/usr/bin/python3
+
+"""First Route
+
+Returns:
+    string: Welcome message
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
+    """Just the first route
+
+    Returns:
+        string: welcome message
+    """
     return "Hello HBNB!"
 
 if __name__ == "__main__":
